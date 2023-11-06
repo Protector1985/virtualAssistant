6d0f8147862e747f74e7a10867c82271a4370f96
Protector1985 <michaelrbraun1985@gmail.com>
2023-11-05 16:45:17 -0800

    basic restaurant function prototype complete

--- src/controllers/callController.ts
+++ src/controllers/callController.ts
@@ -7,22 +7,28 @@ class CallController extends SpeechService {
     public basePath = '/calls'
     public router: Router = express.Router();
     public mainLanguageModel: any
+    public fromNumber: string;
+    public toNumber: string;
     constructor() {
         super();
         this.initRoutes();
+        this.fromNumber=""
+        this.toNumber=""
     }
 
     public initRoutes(): void {
         //receives webhook to start response logic.
         this.router.post(this.basePath + '/call', this.callProcessor.bind(this));
+        this.router.get(this.basePath + '/', (res:Response)=> res.send("API online!"));
     }
 
     
 
     public async callProcessor(req: Request, res: Response) {
         const data = req.body;
+        
         try {
-       
+          
         if(data.data.event_type === "call.playback.started") { 
           this.stopTranscription(data.data.payload.call_control_id)
           
@@ -36,8 +42,31 @@ class CallController extends SpeechService {
         if(data.data.event_type === "call.transcription") { 
           
           const promptToSpeak = await this.mainModelPrompt(data.data.payload.transcription_data.transcript)
-         
-          this.talk(data.data.payload.call_control_id, promptToSpeak)
+          
+          if(promptToSpeak.includes("MENU_REQUESTED")) {
+              let triggerToRemove = "MENU_REQUESTED";
+              let modifiedString = promptToSpeak.replace(new RegExp(triggerToRemove, 'g'), "");
+              this.talk(data.data.payload.call_control_id, modifiedString.trim())
+              console.log(this.fromNumber)
+              console.log(this.toNumber)
+              this.sendTextMessage(this.fromNumber, this.toNumber)
+
+          } else if(promptToSpeak.includes("PERSON_REQUESTED")){
+              let triggerToRemove = "PERSON_REQUESTED";
+              let modifiedString = promptToSpeak.replace(new RegExp(triggerToRemove, 'g'), "");
+              await this.talk(data.data.payload.call_control_id, modifiedString)
+              await this.transferCall(data.data.payload.call_control_id, "+13234253411")
+              this.stopAIAssistant(data.data.payload.call_control_id)
+              this.stopTranscription(data.data.payload.call_control_id)
+          } else if(promptToSpeak.includes("RESERVATION_REQUESTED_BY_USER")) {
+              let triggerToRemove = "RESERVATION_REQUESTED_BY_USER";
+              let modifiedString = promptToSpeak.replace(new RegExp(triggerToRemove, 'g'), "");
+              this.talk(data.data.payload.call_control_id, modifiedString)
+              console.log("Reservation requested.")
+          } else {
+              this.talk(data.data.payload.call_control_id, promptToSpeak)
+          }
+          
           res.send("OK")
         }
         if(data.data.event_type === "call.initiated") {
@@ -47,6 +76,8 @@ class CallController extends SpeechService {
          res.send("OK")
         }
         if (data.data.event_type === 'call.answered') {
+            this.fromNumber=data.data.payload.to
+            this.toNumber=data.data.payload.from
             const initMessage = this.mainLanguageModel?.choices[0]?.message?.content
             this.talk(data.data.payload.call_control_id, initMessage)
             res.send("OK")
@@ -84,7 +115,7 @@ class CallController extends SpeechService {
         );
         
         const data = await resp.json();
-      
+        return data
       } catch(err) {
         console.log(err)
       }
@@ -125,8 +156,89 @@ async stopTranscription(callControlId: string) {
         } catch(err) {
           console.log(err)
         }
-      
       }
+
+    async stopAIAssistant(callControlId:string) {
+      const call = new telnyx.Call({ call_control_id: callControlId });
+
+      try {
+        call.playback_start();
+      } catch(err) {
+        console.log(err)
+      }
+    }
+
+      async sendTextMessage(from:string, to:string) {
+        const apiUrl = 'https://api.telnyx.com/v2/messages';
+        const telnyxApiKey = process.env.TELNYX_API_KEY; // Using the API Key from .env file
+        
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${telnyxApiKey}`
+        };
+
+        const body = JSON.stringify({
+            'from': from,
+            'to': to,
+            'text': 'You can find the menu for Mr Chow here: https://www.mrchow.com/order-online/ '
+        });
+
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: headers,
+                body: body
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                console.log('Message sent successfully:', data);
+            } else {
+                console.error('Error sending message:', data);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    async transferCall(callControlId:string, destination:string) {
+      const apiUrl = `https://api.telnyx.com/v2/calls/${callControlId}/actions/transfer`;
+      const telnyxApiKey = process.env.TELNYX_API_KEY; // Using the API Key from .env file
+
+      const headers = {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${telnyxApiKey}`
+      };
+
+      const body = JSON.stringify({
+          'to': destination, // The destination number to transfer the call to
+      });
+
+      try {
+          const response = await fetch(apiUrl, {
+              method: 'POST',
+              headers: headers,
+              body: body
+          });
+
+          const data = await response.json();
+
+          if (response.ok) {
+              console.log('Call transferred successfully:', data);
+          } else {
+              console.error('Error transferring call:', data);
+          }
+      } catch (error) {
+          console.error('Error:', error);
+      }
+  }
+
+  
 }
+
+
+
 
 export default CallController;
--- src/services/prompts/PromptService.ts
+++ src/services/prompts/PromptService.ts
@@ -3,58 +3,64 @@ import OpenAI from "openai";
 import PhoneService from "../phoneCalls/PhoneService";
 const voice: any = require('elevenlabs-node');
 
-class Promptservice extends PhoneService {
+class PromptService extends PhoneService {
     
-    public openai:any
+    public openai: any;
+    private conversationHistory: { role: string; content: string; }[];
+
     constructor() {
         super();
-        
+        this.conversationHistory = []; // Initialize the conversation history
     }
-    async mainModelPrompt(prompt:string) {
-        
-        try {
-            //defined the api key so that it will be picked up automatically
-            
-                const convo = await this.openai.chat.completions.create({
-                  model: 'gpt-3.5-turbo',
-                  max_tokens: 50,
-                  temperature: 0.1,
-                  messages: [
-                    { role: 'user', content: prompt + "Give me a short response with 50 tokens max."},
 
-                ],
-                });
+    async mainModelPrompt(prompt: string) {
+        try {
+           
             
+            // Add the user's prompt to the conversation history
+            this.conversationHistory.push({ role: 'user', content: prompt });
+
+            const convo = await this.openai.chat.completions.create({
+                model: 'gpt-4',
+                max_tokens: 100,
+                temperature: 0.3,
+                messages: this.conversationHistory, // Pass the conversation history
+            });
+
+            // Add the model's response to the conversation history
+            if (convo.choices[0].message.content) {
+                this.conversationHistory.push({ role: 'assistant', content: convo.choices[0].message.content});
+            }
             
-            return convo?.choices[0]?.message?.content || ""
+            return convo?.choices[0]?.message?.content || "";
            
         } catch(err) {
-            console.log(err)
+            console.log(err);
         }   
     }
 
-
     async initMainModel() {
         try {
-            //defined the api key so that it will be picked up automatically
             this.openai = new OpenAI();
-                const convo = await this.openai.chat.completions.create({
-                  model: 'gpt-3.5-turbo',
-                  max_tokens: 100,
-                  temperature: 0.5,
-                  messages: [
-                    { role: 'system', content: `${process.env.TEST_TRAINING_PROMPT}`},
-
+            const convo = await this.openai.chat.completions.create({
+                model: 'gpt-4',
+                max_tokens: 100,
+                temperature: 0.4,
+                messages: [
+                    { role: 'system', content: `Initiate the client interaction protocol as the virtual assistant for Mr Chow’s restaurant. Start the conversation with a warm greeting, then proceed directly to the first question, waiting for the client's response before moving to the next. Ask one question after the other and wait for the user to respond. Don’t go off topic. If a user asks something that you can’t answer in the context of Mr Chow’s restaurant then offer to have them transferred to a real person. You are not associated with OpenAI. You are the virtual assistant for Mr Chow and nothing else. Don’t give responses that don’t have to do with Mr Chow’s restaurant. Start the client interaction protocol right away. Wait for the user to respond after your prompts. When a user wants to see the menu, offer to send a text message (don’t ask for the phone number) with the menu and wait for the user response. If the menu is desired, send MENU_REQUESTED (all upper case)  headline followed by a short appropriate message to keep the conversation going. If a user asks to speak to a real person or if the user seems to have a concern say  PERSON_REQUESTED (all upper case), kindly inform him that he will be connected to a staff member. Keep all responses short and sweet within 50 tokens.`},
                 ],
-                });
-            
+            });
 
-            return convo
+            // Add the initial system message to the conversation history
+            this.conversationHistory.push({ role: 'system', content: `Initiate the client interaction protocol as the virtual assistant for Mr Chow’s restaurant. Start the conversation with a warm greeting, then proceed directly to the first question, waiting for the client's response before moving to the next. Ask one question after the other and wait for the user to respond. Don’t go off topic. If a user asks something that you can’t answer in the context of Mr Chow’s restaurant then offer to have them transferred to a real person. You are not associated with OpenAI. You are the virtual assistant for Mr Chow and nothing else. Don’t give responses that don’t have to do with Mr Chow’s restaurant. Start the client interaction protocol right away. Wait for the user to respond after your prompts. When a user wants to see the menu, offer to send a text message (don’t ask for the phone number) with the menu and wait for the user response. If the menu is desired, send MENU_REQUESTED (all upper case)  headline followed by a short appropriate message to keep the conversation going. If a user asks to speak to a real person or if the user seems to have a concern say  PERSON_REQUESTED (all upper case), kindly inform him that he will be connected to a staff member. Keep all responses short and sweet within 50 tokens.`})
+            this.conversationHistory.push({ role: 'assistant', content: convo.choices[0].message.content + "Keep response short within 50 tokens. Don't start the sentence with Assistant - just the description of what you want to say." });
+
+            return convo;
            
         } catch(err) {
-            console.log(err)
+            console.log(err);
         }   
     }
 }
 
-export default Promptservice
+export default PromptService;
