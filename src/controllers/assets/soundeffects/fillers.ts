9d23ac6e69e32c378791a710f7855407326654cb
Protector1985 <michaelrbraun1985@gmail.com>
2023-11-24 08:01:00 -0800

    updated prompt

--- src/controllers/assets/soundeffects/fillers.ts
+++ src/controllers/assets/soundeffects/fillers.ts
@@ -1,5 +1,5 @@
 const natashaObj = {
-    // smackuhm:"eaf0ca01-0774-4bd5-b57d-68fec3eb4fc1",
+    smackuhm:"eaf0ca01-0774-4bd5-b57d-68fec3eb4fc1",
     uhm:"bcf6e44d-94b8-43a8-9627-b4168dd4ae9a",
     absolutely: "05e4a486-e243-46dc-b67e-83f2083a435b",
     yep: "393c1509-fe54-4e10-b33a-c61c0f804aba",
